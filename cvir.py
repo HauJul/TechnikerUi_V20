0b5cd17f2ad6d27eef8019b7dfb2bf9ce5878005
julika <julika.haubner@gmail.com>
2023-12-11 14:22:34 +0100

    Solved Qt Storage Errors Exit Code 134

--- cvir.py
+++ cvir.py
@@ -1,44 +1,46 @@
 from gpiozero import DigitalInputDevice, DigitalOutputDevice
 
 #Inputs CVIR2
-__inZYK1 = DigitalOutputDevice(0)
-__inZYK2 = DigitalOutputDevice(1)
-__inZYK4 = DigitalOutputDevice(2)
-__inSPFREI = DigitalOutputDevice(3)
-__inNIOQUT = DigitalOutputDevice(4)
-__inSTART = DigitalOutputDevice(5)
-__inLOESEN = DigitalOutputDevice(6)
-__inRESET = DigitalOutputDevice(7)
+inZYK1 = DigitalOutputDevice(0)
+inZYK2 = DigitalOutputDevice(1)
+inZYK4 = DigitalOutputDevice(2)
+inSPFREI = DigitalOutputDevice(3)
+inNIOQUT = DigitalOutputDevice(4)
+inSTART = DigitalOutputDevice(5)
+inLOESEN = DigitalOutputDevice(6)
+inRESET = DigitalOutputDevice(7)
 
 #Outputs CVIR 2
-__outZYK1 = DigitalInputDevice(8, pull_up=True)
-__outZYK2 = DigitalInputDevice(9, pull_up=True)
-__outZYK4 = DigitalInputDevice(10, pull_up=True)
-__outBEREIT = DigitalInputDevice(11, pull_up=True)
-__outZLAEUF = DigitalInputDevice(12, pull_up=True)
-__outIO = DigitalInputDevice(13, pull_up=True)
-__outNIO = DigitalInputDevice(14, pull_up=True)
-__outIOZ = DigitalInputDevice(15, pull_up=True)
+outZYK1 = DigitalInputDevice(8, pull_up=True)
+outZYK2 = DigitalInputDevice(9, pull_up=True)
+outZYK4 = DigitalInputDevice(10, pull_up=True)
+outBEREIT = DigitalInputDevice(11, pull_up=True)
+outZLAEUF = DigitalInputDevice(12, pull_up=True)
+outIO = DigitalInputDevice(13, pull_up=True)
+outNIO = DigitalInputDevice(14, pull_up=True)
+outIOZ = DigitalInputDevice(15, pull_up=True)
 
-__outSF1 = DigitalInputDevice(19, pull_up=True)
-__outSF2 = DigitalInputDevice(20, pull_up=True)
+outSF1 = DigitalInputDevice(19, pull_up=True)
+outSF2 = DigitalInputDevice(20, pull_up=True)
 
 step = 0
 
 
+
+
 def set_input(inzyk1, inzyk2, inzyk4):
     if inzyk1:
-        __inZYK1.on()
+        inZYK1.on()
     else:
-        __inZYK1.off()
+        inZYK1.off()
     if inzyk2:
-        __inZYK2.on()
+        inZYK2.on()
     else:
-        __inZYK2.off()
+        inZYK2.off()
     if inzyk4:
-        __inZYK4.on()
+        inZYK4.on()
     else:
-        __inZYK4.off()
+        inZYK4.off()
 
 
 def set_cyc(cyc):
@@ -55,32 +57,39 @@ def set_cyc(cyc):
 
 
 def release_start():
-    __inSPFREI.on()
+    inSPFREI.on()
 
 
 def lock_start():
-    __inSPFREI.off()
+    inSPFREI.off()
 
 
 def button_press():
-    state_sf1 = __outSF1.value
-    state_sf2 = __outSF2.value
+    state_sf1 = outSF1.value
+    state_sf2 = outSF2.value
     if state_sf2 and state_sf1:
-        __inRESET.on()
-        __inSTART.off()
-        __inLOESEN.off()
+        inRESET.on()
+        inSTART.off()
+        inLOESEN.off()
     elif state_sf1 and not state_sf2:
-        __inSTART.on()
+        inSTART.on()
     elif state_sf2 and not state_sf1:
-        __inLOESEN.on()
+        inLOESEN.on()
 
 
 def button_release():
-    state_sf1 = __outSF1.value
-    state_sf2 = __outSF2.value
+    state_sf1 = outSF1.value
+    state_sf2 = outSF2.value
     if not state_sf1:
-        __inSTART.off()
-        __inRESET.off()
+        inSTART.off()
+        inRESET.off()
     if not state_sf2:
-        __inLOESEN.off()
-        __inRESET.off()
+        inLOESEN.off()
+        inRESET.off()
+
+
+# Interupt Bedienelement
+outSF1.when_activated = button_press
+outSF1.when_deactivated = button_release
+outSF2.when_activated = button_press
+outSF2.when_deactivated = button_release
--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 # This Python file uses the following encoding: utf-8
 import sys
-import pandas as pd
-from PySide6.QtWidgets import QApplication, QMainWindow, QDialog, QMessageBox, QDialogButtonBox, QTableWidgetItem
-from PySide6.QtCore import Qt
+
+from PySide6.QtWidgets import QApplication, QMainWindow
 
 from UI.ui_mainwindow import Ui_MainWindow
-from UI.ui_settingwindow import Ui_SettingWindow
-from UI.ui_pwdialog import Ui_PasswordDialog
-from UI.ui_newdialog import Ui_NewDialog
+from clsSettingWindow import SettingWindow, PasswordDialog
 
 import cvir
 from toolbox import Toolbox
@@ -39,7 +36,7 @@ class MainWindow(QMainWindow):
             self.ui.txt_process.setText("")
 
     def open_settings(self):
-        password_dialog = PasswordDialog()
+        password_dialog = PasswordDialog(setting_window)
         password_dialog.exec()
 
     def false_tool(self):
@@ -49,115 +46,6 @@ class MainWindow(QMainWindow):
     def right_tool(self):
         self.ui.lbl_process_state.setText("Schrauben starten!")
         cvir.release_start()
-
-
-class SettingWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_SettingWindow()
-        self.ui.setupUi(self)
-        self.read_all()
-        self.ui.btn_close.clicked.connect(self.close)
-        self.ui.btn_save.clicked.connect(self.save_all)
-        self.ui.btn_delete.clicked.connect(self.delete_row)
-        self.ui.btn_new.clicked.connect(self.new_item)
-
-    def new_item(self):
-        new_dialog = NewDialog()
-        new_dialog.exec()
-
-    def read_csv_file(self, filepath, table):
-        file = open(filepath, 'r')
-        df = pd.read_csv(file, delimiter=";", index_col=0)
-        file.close()
-        headers = list(df)
-        index = df.index.tolist()
-        table.setRowCount(df.shape[0])
-        table.setColumnCount(df.shape[1])
-        table.setHorizontalHeaderLabels(headers)
-        table.setVerticalHeaderLabels(map(str, index))
-
-        df_array = df.values
-        for row in range(df.shape[0]):
-            for col in range(df.shape[1]):
-                table.setItem(row, col, QTableWidgetItem(str(df_array[row, col])))
-    def read_all(self):
-        self.read_csv_file("Lists/Produktliste_V03.csv", self.ui.tbl_prod)
-        self.read_csv_file("Lists/Toolliste_V03.csv", self.ui.tbl_tool)
-        self.read_csv_file("Lists/Programmliste_V03.csv", self.ui.tbl_prog)
-
-    def save_csv_file(self, filepath, table):
-        headers = []
-        index = []
-
-        for i in range(table.model().columnCount()):
-            headers.append(table.horizontalHeaderItem(i).text())
-        for j in range(table.model().rowCount()):
-            index.append(table.verticalHeaderItem(j).text())
-
-        df = pd.DataFrame(columns=headers, index=index)
-
-        for row in range(table.rowCount()):
-            for col in range(table.columnCount()):
-                df.at[index[row], headers[col]] = table.item(row, col).text()
-
-        df.to_csv(filepath, sep=";")
-
-    def save_all(self):
-        self.save_csv_file("Lists/Produktliste_V03.csv", self.ui.tbl_prod)
-        self.save_csv_file("Lists/Toolliste_V03.csv", self.ui.tbl_tool)
-        self.save_csv_file("Lists/Programmliste_V03.csv", self.ui.tbl_prog)
-        product.read_csv()
-
-    def delete_row(self):
-        index = self.ui.tbl_prod.selectionModel().selectedIndexes()
-        if not len(index) == 0:
-            self.ui.tbl_prod.removeRow(index[0].row())
-        index = self.ui.tbl_tool.selectionModel().selectedIndexes()
-        if not len(index) == 0:
-            self.ui.tbl_tool.removeRow(index[0].row())
-        index = self.ui.tbl_prog.selectionModel().selectedIndexes()
-        if not len(index) == 0:
-            self.ui.tbl_prog.removeRow(index[0].row())
-
-
-class PasswordDialog(QDialog):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.ui = Ui_PasswordDialog()
-        self.ui.setupUi(self)
-        self.ui.buttonBox.button(QDialogButtonBox.Ok).clicked.connect(self.handleLogin)
-
-    def handleLogin(self):
-        if self.ui.txt_password.text() == '1000':
-            setting_window.read_all()
-            setting_window.show()
-        else:
-            QMessageBox.warning(
-                self, 'Error', 'Falsches Servicepassword')
-
-
-class NewDialog(QDialog):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.ui = Ui_NewDialog()
-        self.ui.setupUi(self)
-        self.ui.btnbox.button(QDialogButtonBox.Ok).clicked.connect(self.handle_new)
-
-    def handle_new(self):
-        new_item = QTableWidgetItem()
-        new_item.setText(self.ui.txt_new.text())
-        match self.ui.comboBox.currentText():
-            case "Neues Produkt":
-                setting_window.ui.tbl_prod.insertRow(setting_window.ui.tbl_prod.rowCount())
-                setting_window.ui.tbl_prod.setVerticalHeaderItem(setting_window.ui.tbl_prod.rowCount()-1, new_item)
-            case "Neuer Einsatz":
-                setting_window.ui.tbl_tool.insertRow(setting_window.ui.tbl_tool.rowCount())
-                setting_window.ui.tbl_tool.setVerticalHeaderItem(setting_window.ui.tbl_tool.rowCount()-1, new_item)
-            case "Neues Programm":
-                setting_window.ui.tbl_prog.insertRow(setting_window.ui.tbl_prog.rowCount())
-                setting_window.ui.tbl_prog.setVerticalHeaderItem(setting_window.ui.tbl_prog.rowCount()-1, new_item)
-
 
 def check_tool():
     if toolbox.get_tool() != product.get_toolno(cvir.step):
@@ -178,12 +66,12 @@ def update_process(step):
 
 
 def io_activated():
-    if cvir.__outIO.value:
+    if cvir.outIO.value:
         window.ui.lbl_cvir_state.setText("In Ordnung")
         next_step()
-    elif cvir.__outNIO.value:
+    elif cvir.outNIO.value:
         window.ui.lbl_cvir_state.setText("Nicht in Ordnung")
-    elif cvir.__outZLAEUF.value:
+    elif cvir.outZLAEUF.value:
         window.ui.lbl_cvir_state.setText("")
         window.ui.lbl_process_state.setText("Schrauber läuft!")
 
@@ -201,20 +89,15 @@ app = QApplication(sys.argv)
 # Start MainWindow
 window = MainWindow()
 window.show()
-setting_window = SettingWindow()
+setting_window = SettingWindow(product)
 # Interupt
 # Toolbox - Tool changed
 toolbox.outBit0.when_activated = check_tool
 toolbox.outBit1.when_activated = check_tool
 toolbox.outBit2.when_activated = check_tool
 # CVIR - Statusmeldung
-cvir.__outIO.when_activated = io_activated
-cvir.__outNIO.when_activated = io_activated
-cvir.__outZLAEUF.when_activated = io_activated
-# Bedienelement
-cvir.__outSF1.when_activated = cvir.button_press
-cvir.__outSF1.when_deactivated = cvir.button_release
-cvir.__outSF2.when_activated = cvir.button_press
-cvir.__outSF2.when_deactivated = cvir.button_release
+cvir.outIO.when_activated = io_activated
+cvir.outNIO.when_activated = io_activated
+cvir.outZLAEUF.when_activated = io_activated
 
 sys.exit(app.exec())
